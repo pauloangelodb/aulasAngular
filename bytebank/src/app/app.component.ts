ceadf299401e075adeb9dca4ddd43d69fa18f411
Paulo Angelo <paulo.barbosa@sga.pucminas.br>
2021-09-21 21:53:23 -0300

    atualiza aulas, e código da tela home do tis 4

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any[] = [];
+
+  transferir($event) {
+    console.log($event);
+    const transfComData = {...$event, data: new Date()};
+    this.transferencia.push(transfComData);
+  }
 }
